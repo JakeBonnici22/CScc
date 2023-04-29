3799d020cdcafe2a68193bfe75bd84417770153d
Jake <jakebon98@gmail.com>
2023-04-29 13:21:53 +0100

    Removed missing values

--- load_preprocessing.py
+++ load_preprocessing.py
@@ -1,5 +1,37 @@
 import pandas as pd
 
 df = pd.read_csv(r'cSCC_data.csv')
-print(df)
 
+'''Create Id column'''
+df["HospitalN"] = df.index + 1
+
+
+'''remove last column 'Unnamed: 17'''
+df.drop(df.columns[[-1]], axis=1, inplace=True)
+
+
+'''Stripping characters'''
+df.iloc[:,1] = df.iloc[:,1].str.strip()
+
+
+'''Remove row which has P in the second column'''
+df = df[df.iloc[:,1] != 'P']
+
+
+'''get unique values of the second column'''
+# print(df.iloc[:,1].unique())
+
+
+'''get count of unique values of the second column'''
+# print(df.iloc[:,1].value_counts())
+
+
+'''display the entire dataframe without truncation'''
+pd.set_option('display.max_columns', None)
+
+'''print rows with missing values in  nulls'''
+# print(df[df.iloc[:,1].isnull()])
+
+'''remove rows with missing values in  nulls'''
+df = df[df.iloc[:,1].notnull()]
+print(df)
