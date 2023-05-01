6886bee200f2e839343b3d6acea0420c63d2dbc9
Jake <jakebon98@gmail.com>
2023-05-01 12:49:02 +0100

    Adding stats (chi2)

--- Stats.py
+++ Stats.py
@@ -1,33 +1,44 @@
 import scipy.stats as stats
 import pandas as pd
+from scipy.stats import chi2_contingency
 
 df = pd.read_csv(r'cSCC_data_clean.csv')
 print(df.columns)
 
+
 # contingency table
 contingency_table = pd.crosstab(df['Sex'], df['Recurrence (0= No recurrence, 1= Recurrence)'])
-
-
 # chi-square test for independence
 chi2, pval, dof, expected = stats.chi2_contingency(contingency_table)
-
-
+print()
 if pval < 0.05:
     print("There is a significant association between Sex and Recurrence.")
 else:
     print("There is no significant association between Sex and Recurrence.")
 
 
+# create a contingency table of death and recurrence
+ct = pd.crosstab(df['Death (0=No death, 1=Death)'], df['Recurrence (0= No recurrence, 1= Recurrence)'])
+# perform a chi-squared test
+chi2, p, dof, expected = chi2_contingency(ct)
+print()
+# print the results
+print(f"chi2 = {chi2:.3f}, p = {p:.3f}")
+if p < 0.05:
+    print("There is a significant association between Death and Recurrence.")
+else:
+    print("There is no significant association between Death and Recurrence.")
 
 
 # split the data into two groups: with recurrence and without recurrence
 recurrence = df[df['Recurrence (0= No recurrence, 1= Recurrence)'] == 1]['Age']
 no_recurrence = df[df['Recurrence (0= No recurrence, 1= Recurrence)'] == 0]['Age']
-
 # t-test for comparing the means of the two groups
 t, pval = stats.ttest_ind(recurrence, no_recurrence)
-
+print()
 if pval < 0.05:
     print("There is a significant difference in Age between patients with and without recurrence.")
 else:
     print("There is no significant difference in Age between patients with and without recurrence.")
+
+
--- logistic_regression.py
+++ logistic_regression.py
@@ -1,17 +1,13 @@
 import statsmodels.api as sm
 import pandas as pd
-import numpy as np
+
 
 df = pd.read_csv(r'cSCC_data_clean.csv')
 print(df['TumourDiamater (mm)'].unique())
 
-
-'''ignore the values in the column "TumourDiamater (mm)" witout "mohs"'''
 df = df[df['TumourDiamater (mm)'] != 'Mohs']
-
-
-# convert the column to a numeric data type
 df['TumourDiamater (mm)'] = pd.to_numeric(df['TumourDiamater (mm)'])
+
 
 # predictors and response variable
 X = df[['Age', 'TumourDepth', 'Grade (1= Well differentiated, 2= moderately differentiated, 3= poorly differentiated)',
