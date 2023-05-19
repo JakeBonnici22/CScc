d7b12be8c58a436778b2a147ecf3edb25f31db77
Jake <jakebon98@gmail.com>
2023-05-19 22:30:05 +0100

    ML adjustments

--- pre_ml.py
+++ pre_ml.py
@@ -28,6 +28,11 @@ pd.set_option('display.max_rows', None)
 
 # Removing selected columns and assigning scaled_data and target_data
 selected_columns = ['HospitalN', 'LRD (1= local recurrence, 2= regional recurrence, 3= distant recurrence)',
-                    'Recurrence (0= No recurrence, 1= Recurrence)', 'Death (0=No death, 1=Death)']
+                    'Recurrence (0= No recurrence, 1= Recurrence)', 'Death (0=No death, 1=Death)', 'x0_Recurrence',
+                    'x0_Primary', 'x1_cheek', 'x1_chest', 'x1_ear', 'x1_eyelid', 'x1_forehead', 'x1_jaw', 'x1_lip',
+                    'x1_lower limb', 'x1_neck', 'x1_nose', 'x1_scalp', 'x1_temple', 'x1_trunk', 'x1_upper limb',
+       'x2_squamous cell carcinoma']
+
 scaled_data = df.drop(selected_columns, axis=1)
 target_data = df['Recurrence (0= No recurrence, 1= Recurrence)']
+print(scaled_data.columns)
